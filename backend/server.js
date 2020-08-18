5853dc51340676a51463352e092426edbbac810c
ThechoiceMaster <68073926+ThechoiceMaster@users.noreply.github.com>
2020-08-18 15:24:18 +0700

    add

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const express = require("express");
+import express from "express";
 import data from "./data";
 
 const app = express();
 
 app.get("/api/product", (req, res) => {
-  res.send(data.product);
+  res.send(data.products);
 });
 
 app.listen(5000, () => {
